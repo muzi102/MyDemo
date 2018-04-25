012ef17060afb1a9d40e6f0f7a81e734031a2a6c
panda102 <1565675791@qq.com>
2018-04-25 09:32:15 +0800

    are you ok?

--- tinkerpatchdemo/src/main/java/com/tinkerpatchdemo/MainActivity.java
+++ tinkerpatchdemo/src/main/java/com/tinkerpatchdemo/MainActivity.java
@@ -1,13 +1,40 @@
 package com.tinkerpatchdemo;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager manager;
+    private BlankFragment blankFragment;
+    private BlankFragment2 blankFragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manager = getFragmentManager();
+        blankFragment = new BlankFragment();
+        blankFragment2 = new BlankFragment2();
+        findViewById(R.id.buttonPanel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                manager.beginTransaction().replace(R.id.search_edit_frame, blankFragment).commit();
+            }
+        });
+        findViewById(R.id.buttonPane2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                manager.beginTransaction().replace(R.id.search_edit_frame, blankFragment2).commit();
+            }
+        });
+        findViewById(R.id.buttonPane3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                manager.beginTransaction().remove(blankFragment).commit();
+                manager.beginTransaction().remove(blankFragment2).commit();
+            }
+        });
     }
 }
